a486556c72adc8b32f8af596e4f481bee0b45964
Milind Gupta <milind.gupta@gmail.com>
2023-11-23 09:55:32 -0800

    Added the initial index and _attr.flakes files for some modules

---
+++ index.lua
@@ -0,0 +1,12 @@
+-- Flakes file list
+
+return {
+	modules = {
+		"all/all/5.3/tableUtils",
+		"Windows/x32/5.3/LuaSec",
+		"Windows/x32/5.3/luasocket",
+	},
+	lua = {
+		"Windows/x32/5.3/Lua"
+	}
+}
