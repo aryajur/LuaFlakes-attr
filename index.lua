ae24ad76b1161e23dcca2ff8a29ac4df9a9bf6ed
Milind Gupta <milind.gupta@gmail.com>
2023-11-24 09:20:00 -0800

    Added diskOP and luafilesystem entries in inde

--- index.lua
+++ index.lua
@@ -5,6 +5,8 @@ return {
 		"all/all/5.1/tableUtils",
 		"all/all/5.2/tableUtils",
 		"all/all/5.3/tableUtils",
+		"all/all/5.3/diskOP",
+		"Windows/x32/5.3/luafilesystem",
 		"Windows/x32/5.3/LuaSec",
 		"Windows/x32/5.3/luasocket",
 	},
