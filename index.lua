d03c3c42086d7c86cb2eb9177ff85d79af4574dc
Milind Gupta <milind.gupta@gmail.com>
2023-11-24 22:03:11 -0800

    Added the oauth2 module and the dependencies json and net.url

--- index.lua
+++ index.lua
@@ -2,10 +2,15 @@
 
 return {
 	modules = {
+		-- Lua Only Modules
 		"all/all/5.1/tableUtils",
 		"all/all/5.2/tableUtils",
 		"all/all/5.3/tableUtils",
 		"all/all/5.3/diskOP",
+		"all/all/5.3/oauth2",
+		"all/all/5.3/json",
+		"all/all/5.3/net",
+		-- Modules that include binary files
 		"Windows/x32/5.3/luafilesystem",
 		"Windows/x32/5.3/LuaSec",
 		"Windows/x32/5.3/luasocket",
